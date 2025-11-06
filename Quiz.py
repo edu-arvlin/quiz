977cb7bdfb4a6449138127d08e39001de401fa7a
edu-arvlin <arvid.lind@skola.taby.se>
2025-11-06 12:46:22 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,12 @@
 # Del 1
 namn = input("Ange ditt namn: ")
 print("Hej " + namn + " välkommen till spelet!")
+
+# Del 2
+import random as rand
+tal = rand.randint(1, 10)
+gissning = int(input("Gissa ett tal mellan 1 och 10: "))
+if gissning == tal:
+    print("Rätt gissat!")
+else:
+    print("Tyvärr, det var " + str(tal))
